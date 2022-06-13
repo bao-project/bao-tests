2cba9d602a0b6ff55dc48f13ddcd07d360c0d871
mafs <miguelafsilva5@gmail.com>
2022-06-13 16:50:31 +0700

    feat: Add reports to misnaming tests/suites, executing no tests
    Add INFO messages (log level 2) when the framework cannot find the test or suite
    required. This is done by modifying two functions to have a return value equal
    to the number of tests executed. One last check was added at the end, to verify
    if any test was executed. Adapt logging macros to be friendlier to use.
    
    Signed-off-by: mafs <miguelafsilva5@gmail.com>

--- src/bao_test.c
+++ src/bao_test.c
@@ -10,6 +10,8 @@
 #define TESTS ""
 #endif
 
+#define DELIMITER _
+
 static const int total_chars = 20;
 unsigned int testframework_tests;
 unsigned int testframework_fails;
@@ -22,36 +24,49 @@ void run_all()
     }
 }
 
-void run_specific_test(char* suite, char* test)
+int run_specific_test(char* suite, char* test)
 {
+    int count = 0;
     struct bao_test* ptr = (struct bao_test*)&testframework_start;
     while (ptr != (struct bao_test*)&testframework_end) {
         if (!strcmp(ptr->suite_name, suite)) {
             if (!strcmp(ptr->test_name, test)) {
                 ptr->func_ptr();
+                count++;
             }
         }
         ptr++;
     }
+    return count;
 }
 
-void run_suite(char* suite)
+int run_suite(char* suite)
 {
+    int count = 0;
     struct bao_test* ptr = (struct bao_test*)&testframework_start;
     while (ptr != (struct bao_test*)&testframework_end) {
         if (strcmp(ptr->suite_name, suite) == 0) {
             ptr->func_ptr();
+            count++;
         }
         ptr++;
     }
+    return count;
 }
 
 void bao_test_entry(void)
 {
     char suites[] = SUITES;
-    char* end = suites + strlen(suites);
-    char* ptr = suites;
+    char* suites_end = suites + strlen(suites);
+    char* suite_ptr = suites;
     char suite[total_chars];
+
+    char tests[] = TESTS;
+    char* tests_end = tests + strlen(tests);
+    char* test_ptr = tests;
+    char test[total_chars];
+
+    int res = 0;
 
     if (strcmp(suites, "all") == 0) {
         run_all();
@@ -65,14 +80,25 @@ void bao_test_entry(void)
         return;
     }
 
-    while (ptr <= end) {
-        sscanf(ptr, "%19s", suite);
-        ptr += strlen(suite) + 1;
-        run_suite(suite);
+    if(strlen(SUITES)!= 0){
+        while (suite_ptr <= suites_end) {
+            sscanf(suite_ptr, "%19s", suite);
+            suite_ptr += strlen(suite) + 1;
+            res =run_suite(suite);
+            if (res == 0){
+                if(TESTF_LOG_LEVEL > 1){
+                    BAO_INFO_TAG();
+                    printf("Suite «%s» does not exist.\n", suite);
+                }
+            }
+        }
     }
 
     if (testframework_tests > 0) {
         BAO_LOG_TESTS();
+    }else{
+        BAO_INFO_TAG();                                 
+        printf("No tests were executed!\n");
     }
     return;
 }
