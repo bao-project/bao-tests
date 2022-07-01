c5fc4803a75d5a709cadcac78b6a3d7ecdc177d8
mafs <miguelafsilva5@gmail.com>
2022-07-01 13:00:30 +0700

    style(cfiles): Add copyright license text
    Signed-off-by: mafs <miguelafsilva5@gmail.com>

--- src/bao_test.c
+++ src/bao_test.c
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) 2021-2023, Bao Project (www.bao-project.com). All rights reserved.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 #include "inc/bao_test.h"
 #include <stdio.h>
 #include <string.h>
