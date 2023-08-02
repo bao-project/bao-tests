6d59a62e95fa63e9b61e984bccdde80435f56df5
Diogo Costa <diogoandreveigacosta@gmail.com>
2023-08-02 07:55:43 -0700

    update(framework): removed useless code
    Signed-off-by: Diogo Costa <diogoandreveigacosta@gmail.com>

--- framework/test_framework.py
+++ framework/test_framework.py
@@ -6,27 +6,10 @@ SPDX-License-Identifier: Apache-2.0
 Test framework main file
 """
 import argparse
-import signal
-import threading
-import time
-import os
-import subprocess
-import shutil
-import uart
 import constants as cons
-import yaml_utils as yutils
-from test_run import Runner
-
 
 PARSER = argparse.ArgumentParser(description="Bao Testing Framework")
-PARSER.add_argument("--platform", help="Test target platform.")
 
 if __name__ == '__main__':
 
     print(cons.BLUE_TEXT + "Framework init..." + cons.RESET_COLOR)
-
-    if os.path.isdir("./__pycache__"):
-        shutil.rmtree("./__pycache__")
-    if os.path.isdir("./.idea"):
-        shutil.rmtree("./.idea")
-
