a636220aca22b50289c631cab5e8fab2c5ff49ae
Miguel Silva <miguelafsilva5@gmail.com>
2024-03-06 15:21:08 +0000

    feat(riscv-launch): Add qemu-riscv64-launcher script
    Signed-off-by: Miguel Silva <miguelafsilva5@gmail.com>

---
+++ framework/launch/qemu-riscv64-virt.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env nix-shell
+#!nix-shell --pure -p qemu -i bash
+
+# Check if a version argument is provided
+if [ -z "$2" ]; then
+    echo "Usage: $0 <qemu-platform> <opensbi_elf_path> <bao-bin-path>"
+    exit 1
+fi
+
+qemu_platform="$1"
+opensbi_elf_path="$2"
+bao_bin_path="$3"
+# -s -S -nographic\
+qemu-system-riscv64 -nographic \
+    -M virt -cpu rv64 -m 4G -smp 4 \
+    -bios $opensbi_elf_path \
+    -device loader,file="$bao_bin_path",addr=0x80200000,force-raw=on \
+    -device virtio-net-device,netdev=net0 \
+    -netdev user,id=net0,net=192.168.42.0/24,hostfwd=tcp:127.0.0.1:5555-:22 \
+    -device virtio-serial-device -chardev pty,id=serial3 -device virtconsole,chardev=serial3 \
+    -serial pty
