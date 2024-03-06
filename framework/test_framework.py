826fa2113ef80af318a8c8b72697f88be0a28fce
Miguel Silva <miguelafsilva5@gmail.com>
2024-03-06 16:12:50 +0000

    ref: Use platform and recipe variables from the command line
    Signed-off-by: Miguel Silva <miguelafsilva5@gmail.com>

--- framework/test_framework.py
+++ framework/test_framework.py
@@ -242,8 +242,8 @@ if __name__ == '__main__':
         recipe = args.recipe
         print("Recipe .nix file: " + recipe)
 
-    BUILD_CMD = 'nix-build ../../' + test_config['nix_file']
-    BUILD_CMD += " --argstr platform " + test_config['platform']
+    BUILD_CMD = 'nix-build ' + recipe
+    BUILD_CMD += " --argstr platform " + platfrm
     BUILD_CMD += " --argstr log_level " + str(args.log_level)
 
     print(BUILD_CMD)
@@ -262,4 +262,4 @@ if __name__ == '__main__':
     move_results_to_output()
 
     print(cons.BLUE_TEXT + "Launching QEMU..." + cons.RESET_COLOR)
-    deploy_test(test_config['platform'])
+    deploy_test(platfrm)
