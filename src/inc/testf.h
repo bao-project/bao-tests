391cd478d792165719a35ab93b690a9b2b8fe1c1
Diogo Costa <diogoandreveigacosta@gmail.com>
2024-03-12 10:17:51 +0000

    fix(logging): add spinlocks to avoid print overlaps
    Signed-off-by: Diogo Costa <diogoandreveigacosta@gmail.com>

--- src/inc/testf.h
+++ src/inc/testf.h
@@ -21,6 +21,9 @@
 
 #include "testf_assert.h"
 #include <stdio.h>
+#include <spinlock.h>
+
+extern spinlock_t print_lock;
 
 extern unsigned int testframework_start, testframework_end;
 
@@ -29,30 +32,38 @@ extern unsigned int testframework_start, testframework_end;
 #define YELLOW()      printf("\033[1;33m")
 #define COLOR_RESET() printf("\033[0m")
 
-#define INFO_TAG()     \
-    YELLOW();          \
-    printf("[INFO] "); \
+#define INFO_TAG()                  \
+    YELLOW();                       \
+    printf("[INFO] ");              \
     COLOR_RESET();
 
-#define FAIL_TAG()        \
-    RED();                \
-    printf("[FAILURE] "); \
+#define FAIL_TAG()                  \
+    RED();                          \
+    printf("[FAILURE] ");           \
     COLOR_RESET();
 
-#define SUCC_TAG()        \
-    GREEN();              \
-    printf("[SUCCESS] "); \
+#define SUCC_TAG()                  \
+    GREEN();                        \
+    printf("[SUCCESS] ");           \
     COLOR_RESET();
 
-#define START_TAG() printf("[TESTF-C] START\n");
+#define START_TAG()                 \
+    spin_lock(&print_lock);         \
+    printf("[TESTF-C] START\n");    \
+    spin_unlock(&print_lock);
 
-#define END_TAG()   printf("[TESTF-C] END\n");
+#define END_TAG()                   \
+    spin_lock(&print_lock);         \
+    printf("[TESTF-C] END\n");      \
+    spin_unlock(&print_lock);
 
 #if (TESTF_LOG_LEVEL > 0)
 #define LOG_FAILURE()                                                 \
     do {                                                              \
+        spin_lock(&print_lock);                                       \
         FAIL_TAG();                                                   \
         printf("\n    File: %s\n    Line: %u\n", __FILE__, __LINE__); \
+        spin_unlock(&print_lock);                                     \
     } while (0)
 #else
 #define LOG_FAILURE()
@@ -75,6 +86,7 @@ extern unsigned int testframework_start, testframework_end;
 #define LOG_TESTS()                                                              \
     do {                                                                         \
         if (TESTF_LOG_LEVEL > 1) {                                               \
+            spin_lock(&print_lock);                                              \
             printf("\n");                                                        \
             INFO_TAG();                                                          \
             printf("Final Report\n");                                            \
@@ -82,9 +94,12 @@ extern unsigned int testframework_start, testframework_end;
                 LOG_NOT_SUCCESS();                                               \
             else                                                                 \
                 LOG_SUCCESS();                                                   \
+            spin_unlock(&print_lock);                                            \
         }                                                                        \
+        spin_lock(&print_lock);                                                  \
         printf("[TESTF-C] TOTAL#%u SUCCESS#%u FAIL#%u\n\n", testframework_tests, \
             testframework_tests - testframework_fails, testframework_fails);     \
+        spin_unlock(&print_lock);                                                \
     } while (0)
 
 #define BAO_TEST(suite, test)                            \
@@ -95,22 +110,28 @@ extern unsigned int testframework_start, testframework_end;
         extern unsigned int testframework_fails;         \
         unsigned char failures = 0;                      \
         if (TESTF_LOG_LEVEL > 1) {                       \
+            spin_lock(&print_lock);                      \
             printf("\n");                                \
             INFO_TAG();                                  \
             printf("Running " #suite "\t" #test "\n");   \
+            spin_unlock(&print_lock);                    \
         }                                                \
         testframework_tests++;                           \
         test_##suite##_##test(&failures);                \
         if (failures) {                                  \
             testframework_fails++;                       \
             if (TESTF_LOG_LEVEL > 1) {                   \
+                spin_lock(&print_lock);                  \
                 FAIL_TAG();                              \
                 printf(#suite "\t" #test " failed! \n"); \
+                spin_unlock(&print_lock);                \
             }                                            \
         } else {                                         \
             if (TESTF_LOG_LEVEL > 1) {                   \
+                spin_lock(&print_lock);                  \
                 SUCC_TAG();                              \
                 printf(#suite "\t" #test " passed! \n"); \
+                spin_unlock(&print_lock);                \
             }                                            \
         }                                                \
     }                                                    \
