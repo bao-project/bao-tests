60ee22a637591cb357d37843dd1cb550c141610f
Diogo Costa <diogoandreveigacosta@gmail.com>
2024-03-27 15:51:30 +0000

    ref(qemu_aarch64_launch): update script do detect launch failure
    Signed-off-by: Diogo Costa <diogoandreveigacosta@gmail.com>

--- framework/launch/qemu-aarch64-virt.sh
+++ framework/launch/qemu-aarch64-virt.sh
@@ -10,11 +10,21 @@ fi
 qemu_platform="$1"
 flash_bin_path="$2"
 bao_bin_path="$3"
-# -s -S -nographic\
-qemu-system-"$qemu_platform" -nographic\
+
+qemu_stderr=$(mktemp)
+
+qemu-system-"$qemu_platform" -nographic \
     -M virt,secure=on,virtualization=on,gic-version=3 \
-   -cpu cortex-a53 -smp 4 -m 4G\
-   -bios $flash_bin_path \
-   -device loader,file="$bao_bin_path",addr=0x50000000,force-raw=on\
-   -device virtio-net-device,netdev=net0 -netdev user,id=net0,hostfwd=tcp:127.0.0.1:5555-:22\
-   -serial pty
+    -cpu cortex-a53 -smp 4 -m 4G \
+    -bios "$flash_bin_path" \
+    -device loader,file="$bao_bin_path",addr=0x50000000,force-raw=on \
+    -device virtio-net-device,netdev=net0 -netdev user,id=net0,hostfwd=tcp:127.0.0.1:5555-:22 \
+    -serial pty 2> "$qemu_stderr"
+
+if grep -q "Could not set up host forwarding rule 'tcp:127.0.0.1:5555-:22'" "$qemu_stderr"; then
+    echo "QEMU produced the specific error message related to host forwarding."
+    exit -1
+fi
+
+rm "$qemu_stderr"
+exit 0
