e5d264fb8d03ee8d4145f9edcb334c98a220645c
Diogo Costa <diogoandreveigacosta@gmail.com>
2024-04-15 16:59:03 +0100

    ref(qemu_riscv64_launch): update script do detect launch failure
    Signed-off-by: Diogo Costa <diogoandreveigacosta@gmail.com>

--- framework/launch/qemu-riscv64-virt.sh
+++ framework/launch/qemu-riscv64-virt.sh
@@ -10,7 +10,13 @@ fi
 qemu_platform="$1"
 opensbi_elf_path="$2"
 bao_bin_path="$3"
-# -s -S -nographic\
+
+if netstat -tuln | grep ":5555 " &>/dev/null; then
+    exit -1
+fi
+
+qemu_stderr=$(mktemp)
+
 qemu-system-riscv64 -nographic \
     -M virt -cpu rv64 -m 4G -smp 4 \
     -bios $opensbi_elf_path \
@@ -18,4 +24,7 @@ qemu-system-riscv64 -nographic \
     -device virtio-net-device,netdev=net0 \
     -netdev user,id=net0,net=192.168.42.0/24,hostfwd=tcp:127.0.0.1:5555-:22 \
     -device virtio-serial-device -chardev pty,id=serial3 -device virtconsole,chardev=serial3 \
-    -serial pty
+    -serial pty 2> "$qemu_stderr"
+    
+rm "$qemu_stderr"
+exit 0
