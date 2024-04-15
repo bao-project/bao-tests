5dc6bbce5d6bdc0cd4f487616433965440892318
Diogo Costa <diogoandreveigacosta@gmail.com>
2024-04-15 13:56:22 +0100

    ref(qemu_aarch64_launch): update script do detect launch failure
    Signed-off-by: Diogo Costa <diogoandreveigacosta@gmail.com>

--- framework/launch/qemu-aarch64-virt.sh
+++ framework/launch/qemu-aarch64-virt.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env nix-shell
-#!nix-shell --pure -p qemu -i bash
+#!nix-shell --pure -p unixtools.netstat -p qemu -i bash
 
 # Check if a version argument is provided
 if [ -z "$2" ]; then
@@ -10,11 +10,20 @@ fi
 qemu_platform="$1"
 flash_bin_path="$2"
 bao_bin_path="$3"
-# -s -S -nographic\
-qemu-system-"$qemu_platform" -nographic\
+
+if netstat -tuln | grep ":5555 " &>/dev/null; then
+    exit -1
+fi
+
+qemu_stderr=$(mktemp)
+
+qemu-system-"$qemu_platform" -nographic \
     -M virt,secure=on,virtualization=on,gic-version=3 \
-   -cpu cortex-a53 -smp 4 -m 4G\
-   -bios $flash_bin_path \
-   -device loader,file="$bao_bin_path",addr=0x50000000,force-raw=on\
-   -device virtio-net-device,netdev=net0 -netdev user,id=net0,hostfwd=tcp:127.0.0.1:5555-:22\
-   -serial pty
+    -cpu cortex-a53 -smp 4 -m 4G \
+    -bios "$flash_bin_path" \
+    -device loader,file="$bao_bin_path",addr=0x50000000,force-raw=on \
+    -device virtio-net-device,netdev=net0 -netdev user,id=net0,hostfwd=tcp:127.0.0.1:5555-:22 \
+    -serial pty 2> "$qemu_stderr"
+
+rm "$qemu_stderr"
+exit 0
