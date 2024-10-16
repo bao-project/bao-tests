4ab6ea81c681698e8a186fb233d9631f06203080
Diogo Costa <diogoandreveigacosta@gmail.com>
2024-10-16 13:48:19 +0100

    fix(testf): fix testf_commands.h format
    Signed-off-by: Diogo Costa <diogoandreveigacosta@gmail.com>

--- src/inc/testf_commands.h
+++ src/inc/testf_commands.h
@@ -7,14 +7,19 @@
 #ifndef TESTF_COMMANDS_H
 #define TESTF_COMMANDS_H
 
-#define PRINT_FUNCTION printf
-#define TESTF_TAG "[TESTF-C] "
+#define PRINT_FUNCTION   printf
+#define TESTF_TAG        "[TESTF-C] "
 
 #define TESTF_COMMAND(X) PRINT_FUNCTION(TESTF_TAG X "\n")
 
-#define COMMAND_START()     PRINT_FUNCTION(TESTF_TAG "START" "\n")
-#define COMMAND_END()       PRINT_FUNCTION(TESTF_TAG "END" "\n")
-#define COMMAND_SEND_CHAR(X) PRINT_FUNCTION(TESTF_TAG "SEND_CHAR " X "\n")
+#define COMMAND_START()              \
+    PRINT_FUNCTION(TESTF_TAG "START" \
+                             "\n")
+#define COMMAND_END()              \
+    PRINT_FUNCTION(TESTF_TAG "END" \
+                             "\n")
+
+#define COMMAND_SEND_CHAR(X)    PRINT_FUNCTION(TESTF_TAG "SEND_CHAR " X "\n")
 #define COMMAND_SEND_TIMEOUT(X) PRINT_FUNCTION(TESTF_TAG "SET_TIMEOUT " X "\n")
 
 #endif // TESTF_COMMANDS_H
